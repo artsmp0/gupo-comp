b966b6c5d9bc8803b3eace43e9d588fa85ef26df
沈孟平 <shen990206@gmail.com>
2023-05-16 11:39:54 +0800

    feat: Successfully run the document project

---
+++ packages/docs/histoire.config.ts
@@ -0,0 +1,53 @@
+import { defaultColors, defineConfig } from 'histoire';
+import { HstVue } from '@histoire/plugin-vue';
+
+export default defineConfig({
+  plugins: [HstVue()],
+  setupFile: './histoire.setup.ts',
+  theme: {
+    title: '古珀组件库',
+    // favicon: 'favicon.svg',
+    logo: {
+      // square: '/src/imgs/logo.png',
+      // light: '/src/imgs/logo.png',
+      // dark: '/src/imgs/logo.png',
+    },
+    colors: {
+      primary: defaultColors.orange,
+    },
+    defaultColorScheme: 'light',
+    hideColorSchemeSwitch: true,
+    storeColorScheme: false,
+  },
+  vite: {
+    server: {
+      host: '0.0.0.0',
+    },
+  },
+  tree: {
+    groups: [
+      {
+        id: 'top',
+        title: '',
+      },
+      {
+        title: 'PC',
+        include: (file) => {
+          return file.path.includes('pc');
+        },
+      },
+      {
+        title: 'Mobile',
+        include: (file) => {
+          return file.path.includes('mobile');
+        },
+      },
+    ],
+  },
+  defaultStoryProps: {
+    icon: 'carbon:assembly-reference',
+    iconColor: '#00c5a5',
+    responsiveDisabled: true,
+    autoPropsDisabled: true,
+  },
+});
---
+++ packages/docs/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      strict: false,
+    },
+  },
+  preview: {
+    host: '0.0.0.0',
+  },
+});
